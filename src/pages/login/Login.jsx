c835245d48392c8887bcd42dbe64b46eed53d411
Elkin-Pena-UV <pena.elkin@correounivalle.edu.co>
2024-09-04 02:47:59 -0500

    Added the Login component with its respective css, and modified the firebase store.

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,25 @@
+import { useCallback } from "react"
+import useAuthStore from "../../stores/use-auth-store"
+import { useEffect } from "react";
+
+export default function Login(){
+
+    const {user,loginGoogleWithPopUp,observeAuthState} = useAuthStore();
+
+    const handleLogin = useCallback(()=>{
+        loginGoogleWithPopUp();
+    },[loginGoogleWithPopUp])
+
+    useEffect(()=>{
+        observeAuthState();
+        console.log(user)
+    }, [observeAuthState,user])
+
+    return (
+        <>
+        <div className="container-login">
+            <button onClick={handleLogin}>Login</button>
+        </div>
+        </>
+    )
+}
