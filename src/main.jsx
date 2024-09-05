f7b82ce93df225b2db173f712329725bf3965544
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-09-05 11:49:27 -0500

    fix: fixed package.json main.jsx Login.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-import './index.css'
-import Sphere from './components/Sphere/Sphere.jsx'
-
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Router from "./routers/Router.jsx";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Sphere/>
-=======
-
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import Router from './routers/Router.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router = {Router}/>
->>>>>>> 1c424525a0589b44509ecd97115e90840febd575
-  </StrictMode>,
-)
+    <RouterProvider router={Router} />
+  </StrictMode>
+);
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,41 +1,37 @@
-import { useCallback } from "react"
-import useAuthStore from "../../stores/use-auth-store"
+import { useCallback } from "react";
+import useAuthStore from "../../stores/use-auth-store";
 import { useEffect } from "react";
 
+export default function Login() {
+  const { user, loginGoogleWithPopUp, observeAuthState } = useAuthStore();
 
-export default function Login(){
+  const handleLogin = useCallback(() => {
+    loginGoogleWithPopUp();
+  }, [loginGoogleWithPopUp]);
 
-    const {user,loginGoogleWithPopUp,observeAuthState,logout} = useAuthStore();
+  useEffect(() => {
+    observeAuthState();
+    console.log(user);
+  }, [observeAuthState, user]);
 
-    const handleLogin = useCallback(()=>{
-        loginGoogleWithPopUp();
-    },[loginGoogleWithPopUp])
+  return (
+    <>
+      <div className="container-login">
+        <div className="row">
+          <div className="row-md-4">
+            <div className="loginpart1">
+              <div className="card card-body">
+                <p>Bienvenido a Textile Purify</p>
 
-    useEffect(()=>{
-        observeAuthState();
-        console.log(user)
-    }, [observeAuthState,user])
-
-    return (
-        <>
-        <div className="container-login">
-            <div className="row">
-                <div className="row-md-4">
-                    <div className="loginpart1">
-                        <div className="card card-body">
-                            <p>
-                                Bienvenido a Textile Purify
-                            </p>
-                            <form>
-                                <button className='btnform' onClick={handleLogin}>Sign-in-with-google</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <div className="row-md-8">
-                </div>
+                <button className="btnform" onClick={handleLogin}>
+                  Sign-in-with-google
+                </button>
+              </div>
             </div>
+          </div>
+          <div className="row-md-8"></div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
