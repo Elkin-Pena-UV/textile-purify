acfa63897ae564b0fb125b74ab5757e411da22bb
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-10-21 18:39:00 -0500

    update:the fish model was updated with the PBR material, the materials folder and the light were included.

--- src/components/Fish/Fish.jsx
+++ src/components/Fish/Fish.jsx
@@ -1,43 +1,48 @@
 /* eslint-disable react/no-unknown-property */
-import { useRef } from "react";
-import { useGLTF, useAnimations } from "@react-three/drei";
-import { useEffect } from "react";
+import {useRef, useEffect,useMemo } from "react";
+import { useGLTF, useAnimations, useTexture } from "@react-three/drei";
+
 
 const Fish = (props) => {
   const group = useRef();
   const { nodes, materials, animations } = useGLTF("models-3d/fish.glb");
   const { actions } = useAnimations(animations, group);
 
+  const PATH = useMemo(() => "/materials/fish/", []);
+
+  const fishTexutre = useTexture({
+     map: PATH + "Basecolor_color.jpeg",
+     normalMap: PATH + "koifishnormal_(1).jpg"
+  });
+  
+ 
+
+  console.log(fishTexutre)
+
   useEffect(() => {
     if (actions && actions["swim"]) {
       actions["swim"].reset().fadeIn(0.5).play();
     }
+    if (actions && actions["tailWiggle"]) {
+      actions["tailWiggle"].reset().fadeIn(0.5).play();
+    }
   }, [actions]);
 
   return (
+    <>
     <group ref={group} {...props} dispose={null}>
-      <group name="Sketchfab_Scene">
-        <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]}>
-          <group name="root">
-            <group name="GLTF_SceneRootNode" rotation={[Math.PI / 2, 0, 0]}>
-              <group
-                name="koifish_0"
-                position={[-0.268, 0, 0]}
-                rotation={[Math.PI, 0, Math.PI]}
-              >
-                <mesh
-                  name="mesh_0"
-                  geometry={nodes.mesh_0.geometry}
-                  material={materials.SimplygonCastMaterial}
-                  morphTargetDictionary={nodes.mesh_0.morphTargetDictionary}
-                  morphTargetInfluences={nodes.mesh_0.morphTargetInfluences}
-                />
-              </group>
-            </group>
-          </group>
-        </group>
+      <group name="Scene">
+        <mesh
+          name="Fish"
+          geometry={nodes.Fish.geometry}
+          material={materials.SimplygonCasatMaterial}
+          position={[-0.268, 0, 0]}
+          rotation={[Math.PI, 0, Math.PI]}
+        />
       </group>
     </group>
+    
+</>
   );
 };
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,9 +5,8 @@ import { Canvas } from "@react-three/fiber";
 import { getDocs, query, where } from "firebase/firestore";
 import UserDao from "../../daos/UserDao";
 import { OrbitControls, PositionalAudio } from "@react-three/drei";
-import Beach from "../../components/Beach/Beach";
-import Turtle from "../../components/Turtle/Turtle";
 import Fish from "../../components/Fish/Fish";
+import LightFish from "./ligth/LigthFish";
 
 const Home = () => {
   const { user, logout } = useAuthStore();
@@ -52,18 +51,11 @@ const Home = () => {
       </button>
 
       <div className="container">
-        <Canvas camera={{ position: [4, 5, 5], fov: 50 }}>
-          <ambientLight intensity={0.3} />
-          <pointLight position={[10, 10, 10]} intensity={.2} />
-          <spotLight
-            position={[-10, 10, 5]}
-            intensity={0.5}
-            angle={0.2}
-            penumbra={0.3}
-          />
-          <directionalLight position={[10, 10, 10]} intensity={2.7} />
-          <OrbitControls />
-          <Beach/>
+        <Canvas camera={{ position: [0, 1, 8] }}>
+          <OrbitControls makeDefault />
+          <LightFish/>
+          <Fish />
+
           <group position={[0, 5, 0]}>
             <PositionalAudio
               autoplay
