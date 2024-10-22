b0640ffde1331eef2bb7f04f79e50055c1f40660
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-10-21 19:09:06 -0500

    feat:the acidity component has been added

---
+++ src/pages/acidity/Acidity.jsx
@@ -0,0 +1,18 @@
+
+import { Canvas } from "@react-three/fiber";
+
+const Acidity = () => {
+
+    return(
+
+        <Canvas>
+
+            
+        </Canvas>
+        
+
+    )
+
+}
+
+export default Acidity;
--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -3,25 +3,44 @@ import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import ProtectedRoute from "./ProtectedRoute";
 import PublicRoute from "./PublicRoute";
-
+import Pollution from "../pages/pollution/Pollution";
+import Acidity from "../pages/acidity/Acidity";
 const Router = createBrowserRouter([
-    {
-      path:"/",
-      element:(  
-        <PublicRoute> <Login/>  </PublicRoute>
-    )
-    },
-    {
-      path:"/home",
-      element:(
-        <ProtectedRoute> <Home/> </ProtectedRoute>
-    )
-    },
-    {
-      path:"/pollution",
-      element:(
-        <ProtectedRoute> <Pollution/> </ProtectedRoute>
-    )
-    }
-  ])
-  export default Router;
+  {
+    path: "/",
+    element: (
+      <PublicRoute>
+        {" "}
+        <Login />{" "}
+      </PublicRoute>
+    ),
+  },
+  {
+    path: "/home",
+    element: (
+      <ProtectedRoute>
+        {" "}
+        <Home />{" "}
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/pollution",
+    element: (
+      <ProtectedRoute>
+        {" "}
+        <Pollution />{" "}
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/acidity",
+    element: (
+      <ProtectedRoute>
+        {" "}
+        <Acidity />{" "}
+      </ProtectedRoute>
+    ),
+  },
+]);
+export default Router;
