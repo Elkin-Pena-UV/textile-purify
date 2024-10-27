6b6ace41f68d6c63715626a6f2968c55c8030074
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-10-27 16:03:32 -0500

    update:Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,8 +9,7 @@ import { OrbitControls, PositionalAudio } from "@react-three/drei";
 import Fish from "../../components/Fish/Fish";
 import LightFish from "./ligths/LigthFish";
 import Header from "../../components/header/Header";
-import Shark from "../../components/Shark/Shark";
-import Beach from "../../components/Beach/Beach";
+
 const Home = () => {
   const { user, logout } = useAuthStore();
 
