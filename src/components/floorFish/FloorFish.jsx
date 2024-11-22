3a5e7434645f41f4739c0477ec44c2482aba0b1a
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-11-22 01:32:27 -0500

    feat: Wrap FloorFish component in RigidBody for enhanced physics interaction

--- src/components/floorFish/FloorFish.jsx
+++ src/components/floorFish/FloorFish.jsx
@@ -1,31 +1,34 @@
 /* eslint-disable react/no-unknown-property */
 import { useTexture } from "@react-three/drei";
+import { RigidBody } from "@react-three/rapier";
 import { useMemo } from "react";
 
-const FloorFish = () => {
-    const PATH = useMemo(() => "materials/floorFish/cliff_side_", []);
-    const floorTexture = useTexture({
-        normalMap: PATH + "nor_gl_1k.jpg",
-        roughnessMap: PATH + "rough_1k.jpg",
-        ambientOcclusionMap: PATH + "ao_1k.jpg",
-        map:PATH + "diff_1k.jpg",
-        displacementMap: PATH + "disp_1k.png"
+const FloorFish = (props) => {
+  const PATH = useMemo(() => "materials/floorFish/cliff_side_", []);
 
-    });
 
-    return (
-        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]} receiveShadow>
-            <planeGeometry args={[100,100]} />
-            <meshStandardMaterial
-                map={floorTexture.map}
-                normalMap={floorTexture.normalMap}
-                roughnessMap={floorTexture.roughnessMap}
-                aoMap={floorTexture.ambientOcclusionMap}
-                displacementMap={floorTexture.displace}
-                receiveShadow
-            /> 
-        </mesh>
-    );
-}
+  const floorTexture = useTexture({
+    normalMap: PATH + "nor_gl_1k.jpg",
+    roughnessMap: PATH + "rough_1k.jpg",
+    ambientOcclusionMap: PATH + "ao_1k.jpg",
+    map: PATH + "diff_1k.jpg",
+    displacementMap: PATH + "disp_1k.png",
+  });
+
+  return (
+    <RigidBody>
+      <mesh {...props} >
+        <planeGeometry args={[100, 100]} />
+        <meshStandardMaterial
+          map={floorTexture.map}
+          normalMap={floorTexture.normalMap}
+          roughnessMap={floorTexture.roughnessMap}
+          aoMap={floorTexture.ambientOcclusionMap}
+          displacementMap={floorTexture.displace}
+        />
+      </mesh>
+      </RigidBody>
+  );
+};
 
 export default FloorFish;
