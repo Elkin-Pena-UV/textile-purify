0ca727039ecf2c4f9079800d514dfb29b378e7dd
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-11-22 23:23:45 -0500

    refactor: Clean up Controls component for improved readability

--- src/components/controls/Controls.jsx
+++ src/components/controls/Controls.jsx
@@ -1,21 +1,14 @@
-
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls } from "@react-three/drei";
 
 const Controls = () => {
-
-
-    return(
-        <OrbitControls
-        minAzimuthAngle={-Math.PI / 2}
-        maxAzimuthAngle={Math.PI / 2}
-        minPolarAngle={0}
-        maxPolarAngle={Math.PI/2}
-        enableZoom={false}
-        enablePan={false}
-
-        />
-    )
-
-}
+  return (
+    <OrbitControls
+      minPolarAngle={0} 
+      maxPolarAngle={Math.PI / 2}
+      enableZoom={false}
+      enablePan={false}
+    />
+  );
+};
 
 export default Controls;
