0e98f9f23ab75b4d1f2b2fb6f022b612fef269bd
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-11-25 17:06:51 -0500

    feat: Add Introduction component with styling and content for Acidity page

---
+++ src/pages/acidity/introduction/Introduction.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Introduction.css";
+
+const Introduction = () => {
+  return (
+    <section className="introduction">
+      <h2 className="introduction-title">Introducción</h2>
+      <p className="introduction-description">
+        La acidificación del agua, medida por su pH, es un proceso crítico que
+        afecta la vida acuática y el equilibrio de los ecosistemas. Un pH
+        equilibrado es esencial para la salud de especies como peces y plantas
+        acuáticas, ya que facilita procesos vitales como la fotosíntesis y la
+        reproducción. Sin embargo, debido a la contaminación y otras actividades
+        humanas, los niveles de acidez en muchas fuentes de agua han aumentado,
+        lo que tiene graves consecuencias. Este aumento de la acidez puede
+        reducir la biodiversidad, alterar las cadenas alimenticias y dañar los
+        ecosistemas acuáticos. Por lo tanto, es crucial monitorear y gestionar
+        adecuadamente los niveles de pH para proteger la salud de nuestros
+        ecosistemas acuáticos.
+      </p>
+    </section>
+  );
+};
+
+export default Introduction;
