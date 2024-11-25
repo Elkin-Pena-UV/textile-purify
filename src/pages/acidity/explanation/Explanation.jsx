da0a57b7531479c49933bec587cbcc84ff85a583
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-11-25 17:00:37 -0500

    feat: Add Explanation component with styling for Acidity page

---
+++ src/pages/acidity/explanation/Explanation.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Explanation.css";
+const Explanation = () => {
+  return (
+    <section className="explanation">
+      <h2 className="explanation-title">¿Qué es la acidificación?</h2>
+      <p className="explanation-description">
+        La acidificación del agua se refiere al proceso mediante el cual el pH
+        del agua disminuye, lo que hace que se vuelva más ácida. Este fenómeno
+        puede ser causado por diversos factores, como la contaminación
+        industrial, la quema de combustibles fósiles y las actividades
+        agrícolas. Un pH bajo puede afectar negativamente la vida acuática y
+        alterar los ecosistemas acuáticos.
+      </p>
+    </section>
+  );
+};
+
+export default Explanation;
