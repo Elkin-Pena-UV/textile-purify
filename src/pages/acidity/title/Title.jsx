d1c1cc4e0d56ed5d22cf0a82db908b39b5f3d86a
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-12-16 02:22:29 -0500

    feat: update Title component to use Poppins Medium Regular font and adjust position and text properties

--- src/pages/acidity/title/Title.jsx
+++ src/pages/acidity/title/Title.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Text3D } from "@react-three/drei";
 
-
 const Title = () => {
   return (
     <Text3D
-      font="../../../../public/fonts/Poppins-Medium.ttf"
-      scale={[0.5, 0.5, 0.5]}
-      position={[-4, 4, 0]}
+      scale={[0.5, 0.5, 0.5]}     
+      position={[-5, 4, 0]}
+      font="../../../../public/fonts/Poppins Medium_Regular.json" 
+      color="#38A3A5" 
+      maxWidth={20} 
+      lineHeight={1.2} 
+      letterSpacing={0.05} 
+      textAlign="center" 
+
     >
       <meshBasicMaterial attach="material" color="#38A3A5" />
       Acidificación del Agua
