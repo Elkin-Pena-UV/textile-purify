eba8b00a95e5e2a0447841cdb2e6b0852740f962
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-12-16 17:06:24 -0500

    feat: enhance Quiz component with additional 3D elements and controls

--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,12 +1,19 @@
-import { Loader } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import River from "../../components/River/River";
+import StadingNight from "../../components/stading/StadingNight";
+import Controls from "../../components/controls/Controls"
+import River from "../../components/river/River";
+import { Loader, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import LightFish from "../../components/ligths/LightFish";
+im
 const Quiz = () => {
   return (
     <Suspense fallback={<Loader />}>
       <Canvas>
-        <River />
+        <River scale={[4,4,4]}/>
+        <StadingNight/>
+        <LightFish/>
+        <Controls/>
       </Canvas>
     </Suspense>
   );
