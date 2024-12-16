b0befd40fb1c6f3cbda6d0901a27c59b22023093
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-12-16 15:41:29 -0500

    fix: update font path in Title component for consistency

--- src/pages/acidity/title/Title.jsx
+++ src/pages/acidity/title/Title.jsx
@@ -4,15 +4,14 @@ import { Text3D } from "@react-three/drei";
 const Title = () => {
   return (
     <Text3D
-      scale={[0.5, 0.5, 0.5]}     
+      scale={[0.5, 0.5, 0.5]}
       position={[-5, 4, 0]}
-      font="../../../../public/fonts/Poppins Medium_Regular.json" 
-      color="#38A3A5" 
-      maxWidth={20} 
-      lineHeight={1.2} 
-      letterSpacing={0.05} 
-      textAlign="center" 
-
+      font="/fonts/Poppins Medium_Regular.json"
+      color="#38A3A5"
+      maxWidth={20}
+      lineHeight={1.2}
+      letterSpacing={0.05}
+      textAlign="center"
     >
       <meshBasicMaterial attach="material" color="#38A3A5" />
       Acidificación del Agua
