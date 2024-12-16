a6e28561c1303326592dc9e45a46337782ecf3fc
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-12-16 15:55:32 -0500

    fix: correct font path in Title component for consistency

--- src/pages/acidity/title/Title.jsx
+++ src/pages/acidity/title/Title.jsx
@@ -6,7 +6,7 @@ const Title = () => {
     <Text3D
       scale={[0.5, 0.5, 0.5]}
       position={[-5, 4, 0]}
-      font="/fonts/Poppins Medium_Regular.json"
+      font="/fonts/Poppins-Medium_Regular.json"
       color="#38A3A5"
       maxWidth={20}
       lineHeight={1.2}
