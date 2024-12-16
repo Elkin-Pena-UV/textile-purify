a39385b99b16e380ae1cfe46488b3c0a38b84f11
SantiagoAG-UV <santiago.alvarez.gutierrez@correounivalle.edu.co>
2024-12-16 02:19:01 -0500

    feat: enhance HammerheadShark component with interactive messages and animations

--- src/components/sharks/HammerheadShark.jsx
+++ src/components/sharks/HammerheadShark.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
+import { Text } from "@react-three/drei";
 
 const HammerheadShark = (props) => {
   const group = useRef();
@@ -9,21 +10,25 @@ const HammerheadShark = (props) => {
 
   const { actions } = useAnimations(animations, group);
 
-  //name of the animations in the gltf file, change the name of the animation to the one you want to use, you must change it in const animation = actions[""];
-  // 1.Attacking
-  // 2.Attacking_shake
-  // 3.Attacking_Shake_Loop
-  // 4.AttackingFromLeft
-  // 5.AttackingFromRight
-  // 6.Idle
-  // 7.Swimming
-  // 8.Swimming_D
-  // 9.Swimming_L
-  // 10.Swimming_U
-  // 11.Swimming_R
+
+
+  //  Name of the animations in the gltf file
+  // 'Attacking'
+  // 'Attacking_shake'
+  //  'Attacking_Shake_Loop'
+  //  'AttackingFromLeft',
+  //   'AttackingFromLeft'
+  //    'AttackingFromLeft'
+  //    'Idle'
+  //    'Swimming'
+  //     'Swimming_D'
+  //      'Swimming_L'
+  //      'Swimming_R'
+  //      'Swimming_U'
 
   useEffect(() => {
     if (actions) {
+      console.log(animations);
       const animation = actions["Swimming"];
       if (animation) {
         animation.play();
@@ -31,15 +36,73 @@ const HammerheadShark = (props) => {
     }
   }, [actions]);
 
+  const [showMessages, setShowMessages] = useState(false);
+  const [messageIndex, setMessageIndex] = useState(0);
+  const [showIntroText, setShowIntroText] = useState(true);
+  const messages = [
+    "¡Hola! Soy Sharky, un tiburón martillo, y hoy te hablaré sobre un problema importante: la acidificación del agua.",
+    "Este problema ocurre cuando el dióxido de carbono (CO2) que liberamos al ambiente se disuelve en el océano.",
+    "Esto hace que el agua se vuelva más ácida, lo que cambia su equilibrio natural y afecta a muchos organismos marinos.",
+    "Corales, moluscos y otros animales marinos luchan para sobrevivir en estas condiciones, lo que pone en riesgo toda la vida en el océano.",
+    "Es crucial entender este problema, ya que nuestros océanos son vitales para la vida en el planeta. ¡Mis amigos y yo te contaremos más!",
+  ];
+
+  const handleClick = () => {
+    if (showMessages) {
+      setShowMessages(false);
+      setMessageIndex(0);
+    } else {
+      setShowMessages(true);
+      setShowIntroText(false);
+    }
+  };
+
+  useEffect(() => {
+    if (showMessages && messageIndex < messages.length) {
+      const timer = setTimeout(() => {
+        setMessageIndex(messageIndex + 1);
+      }, 5000);
+      return () => clearTimeout(timer);
+    } else if (messageIndex === messages.length) {
+      setShowIntroText(true);
+    }
+  }, [showMessages, messageIndex]);
+
+  const handlePointerOver = () => {
+    document.body.style.cursor = "pointer";
+  };
+
+  const handlePointerOut = () => {
+    document.body.style.cursor = "default";
+  };
+
   return (
     <group
       ref={group}
       {...props}
       dispose={null}
-      scale={[0.1, 0.1, 0.1]}
+      scale={[0.3, 0.3, 0.3]}
       castShadow
       receiveShadow
+      onClick={handleClick}
+      position={[0 - 3, 2, 0]}
+      onPointerOver={handlePointerOver}
+      onPointerOut={handlePointerOut}
     >
+      {showIntroText && (
+        <group position={[0, 3, 0]}>
+          <Text
+            fontSize={0.5}
+            anchorX="center"
+            anchorY="middle"
+            strokeWidth={0.1}
+            strokeColor="black"
+          >
+            ¡Hazme clic, tengo algo importante que decirte!
+          </Text>
+        </group>
+      )}
+
       <group name="Scene" castShadow receiveShadow>
         <group name="HeadHammerSharkRig" castShadow receiveShadow>
           <skinnedMesh
@@ -54,6 +117,28 @@ const HammerheadShark = (props) => {
           <primitive object={nodes["ORG-torso"]} castShadow receiveShadow />
         </group>
       </group>
+
+      {showMessages && messageIndex < messages.length && (
+        <group position={[6, 1, 0]}>
+          <mesh position={[0, 0, -0.1]}>
+            <boxGeometry args={[7, 5, 0.1]} />
+            <meshStandardMaterial color="black" />
+          </mesh>
+          <Text
+            fontSize={0.5}
+            color="white"
+            anchorX="center"
+            anchorY="middle"
+            emissive="black"
+            strokeWidth={0}
+            strokeColor="black"
+            maxWidth={6}
+            emissiveIntensity={1}
+          >
+            {messages[messageIndex]}
+          </Text>
+        </group>
+      )}
     </group>
   );
 };
