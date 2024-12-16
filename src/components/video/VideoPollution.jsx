7bdc3f1dfeb769edaec0e10b07929c1594b821ea
Elkin-Pena-UV <pena.elkin@correounivalle.edu.co>
2024-12-16 16:55:21 -0500

    feat: add Pirate and Pirate2 components with 3D models; update video components for sound control

--- src/components/video/VideoPollution.jsx
+++ src/components/video/VideoPollution.jsx
@@ -1,17 +1,36 @@
-import { useVideoTexture } from "@react-three/drei"
+import { useVideoTexture } from "@react-three/drei";
+import { useCallback, useState } from "react";
 
 const VideoPollution = (props) => {
-    const texture = useVideoTexture("/videos/video_pollution.mp4", {
-        loop: true,
-        start: true
-    });
+  const texture = useVideoTexture("/videos/video_pollution.mp4", {
+    muted: false,
+    loop: true,
+    start: false,
+  });
 
-    return (
-        <mesh {...props} position={[0.7, 0.35 ,0]} scale={0.2} rotation={[-Math.PI / -0.2, -0.4, 9.43]}>
-            <planeGeometry args={[2,1]}/>
-            <meshBasicMaterial map={texture} toneMapped={false}/>
-        </mesh>
-    );
+  const [pause, setPause] = useState(false);
+
+  const handleVideo = useCallback(
+    (e) => {
+      e.stopPropagation();
+      pause ? texture.image.play() : texture.image.pause();
+      setPause(!pause);
+    },
+    [pause, setPause, texture]
+  );
+
+  return (
+    <mesh
+      {...props}
+      position={[0.7, 0.35, 0]}
+      scale={0.2}
+      rotation={[-Math.PI / -0.2, -0.4, 9.43]}
+      onClick={handleVideo}
+    >
+      <planeGeometry args={[2, 1]} />
+      <meshBasicMaterial map={texture} toneMapped={false} />
+    </mesh>
+  );
 };
 
 export default VideoPollution;
